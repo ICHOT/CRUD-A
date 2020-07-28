1aced9b3ed1ac1869c8b744ad143e552ea126366
ICHOT <64571503+ICHOT@users.noreply.github.com>
2020-07-28 14:17:56 +0700

    A-1

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
     }
     ?>
   </table>
+
 </body>
 
 </html>
--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,6 @@
 <?php
 $koneksi = mysqli_connect("localhost", "root", "", "ICHOT");
 
-
 if (mysqli_connect_errno()) {
   echo "Koneksi database gagal : " . mysqli_connect_error();
 }
